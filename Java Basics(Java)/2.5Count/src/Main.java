a4ba055584ae6312182ec9a3fd258577e1f10a09
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2021-04-29 08:12:27 -0500

    Delete Java Basics(Java) directory

--- Java Basics(Java)/2.5Count/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-
-    public static void main(String[] args) {
-    	// c instance only allowed within main method
-        Counter c = new Counter();
-        c.incrementCount(5);
-        System.out.println(c.count);
-    }
-}
