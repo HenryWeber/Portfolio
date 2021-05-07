50a175824368e13ffa6c8985882cce9979c48125
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2021-05-07 08:03:09 -0500

    Update Main.java

--- GaussianBlur (Java)/src/Main.java
+++ GaussianBlur (Java)/src/Main.java
@@ -7,7 +7,9 @@ import javax.imageio.ImageIO;
 public class Main {
 
 	public static void main(String[] args) {
-			
+			/*
+			Demo Input: [source_image2.jpg]-[31]-[5]
+			*/
 			InputWindow inputw = new InputWindow();
 			Sequencer s = new Sequencer();
 			FileManager fm = new FileManager(inputw);
